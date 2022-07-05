1a9ca7937b79e9ff8b93130cc30551744b278824
linkaiyan <1760942033@qq.com>
2022-07-05 15:19:06 +0800

    生产环境代码压缩

--- craco.config.js
+++ craco.config.js
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: linkaiyan
  * @Date: 2022-02-18 14:21:17
- * @LastEditTime: 2022-07-05 11:48:01
+ * @LastEditTime: 2022-07-05 15:17:43
  * @LastEditors: linkaiyan
  * @Reference:
  */
@@ -11,6 +11,8 @@
 /* craco.config.js */
 const CracoLessPlugin = require('craco-less');
 const SimpleProgressWebpackPlugin = require( 'simple-progress-webpack-plugin' )
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const isProd = process.env.REACT_APP_ENV === 'production';
 
 const config = {
   webpack: {
@@ -43,6 +45,22 @@ const config = {
       },
     },
   ]
+}
+
+if(isProd) {
+  config.webpack.plugins.push(
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        compress: {
+          warnings: false,
+          drop_debugger: true,
+          drop_console: true,
+        },
+      },
+      sourceMap: false,
+      parallel: true,
+    })
+  )
 }
 
 module.exports = config;
